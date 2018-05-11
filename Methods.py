1827c9111a76191b5213297dbdbf9c758c94500e
Jenny Fish <jfish2018@pingry.org>
2018-05-11 14:22:52 -0400

    fixed self

--- Methods.py
+++ Methods.py
@@ -4,17 +4,17 @@ import json
 class Assignment:
 	
 	def __init__(self, username, repoName):
-		this.username = username
-		this.repoName = repoName
+		self.username = username
+		self.repoName = repoName
 	
 	#returns the total number of commits for the assignment repo
-	def getNumberOfCommits():
+	def getNumberOfCommits(self):
 
 	#returns a list containing the dates of each commit
-	def getDatesOfCommits():
+	def getDatesOfCommits(self):
 
 	#returns the number of commits between two given dates
-	def numCommitsDateRange(startDate, endDate):
+	def numCommitsDateRange(self, startDate, endDate):
 
 	# returns a string that indicates if the student is up to date, behind, etc
-	def statusMessage():
+	def statusMessage(self):
