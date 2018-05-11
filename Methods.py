35f064083983ede17c8b05f34bb8280e3bcbef0f
Ami <agianchandani2018@pingry.org>
2018-05-11 14:17:38 -0400

    Methods start

---
+++ Methods.py
@@ -0,0 +1,15 @@
+import requests
+import json
+
+
+
+def getNumberOfCommits(username, repoName):
+
+
+def getDatesOfCommits(username, repoName):
+
+
+def numCommitsDateRange(username, repoName, startDate, endDate):
+
+# returns a stirng that indicates if the student is up to date, behind, etc
+def statusMessage()
