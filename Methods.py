948ea7f7c72d4dae2e04945d5852608fe840c96f
Jenny Fish <jfish2018@pingry.org>
2018-05-13 23:52:21 -0400

    finished some methods

--- Methods.py
+++ Methods.py
@@ -3,18 +3,50 @@ import json
 
 class Assignment:
 	
+	'''
+	Fields:
+	username - the owner of the repository
+	repoName - the name of the repository to fetch info on
+	dec - a json decoder
+	repo - the json object of the repo
+	'''
+	
 	def __init__(self, username, repoName):
 		self.username = username
 		self.repoName = repoName
+		self.dec = json.JSONDecoder()
+		
+		#get the json object for the user info
+		self.repo = self.dec.decode(requests.get('https://api.github.com/', auth=(username, '')).text)
+		#get the json object for the repository
+		self.repo = self.dec.decode(requests.get(self.repo["repository_url"].replace("{owner}", username).replace("{repo}", repoName), "").text)
+		
+		#later: error handling?
+		
+	#returns a list of all commits for the repository
+	def getCommitList(self):
+		c = self.repo["commits_url"].replace("{/sha}", "")
+		c = requests.get(c, "")
+		return self.dec.decode(c.text)
 	
 	#returns the total number of commits for the assignment repo
 	def getNumberOfCommits(self):
-
+		return len(self.getCommitList())
+	
 	#returns a list containing the dates of each commit
 	def getDatesOfCommits(self):
+		li = []
+		for commit in self.getCommitList():
+			li.append(commit["commit"]["committer"]["date"])
+		return li
 
 	#returns the number of commits between two given dates
 	def numCommitsDateRange(self, startDate, endDate):
+		pass
 
 	# returns a string that indicates if the student is up to date, behind, etc
 	def statusMessage(self):
+		pass
+
+a = Assignment('jennyafish', 'PrintStarsHW')
+print(a.getDatesOfCommits())
