c1f3a187346795c386b6f5abac4992120d389574
Jenny Fish <jfish2018@pingry.org>
2018-05-23 12:20:42 -0400

    ugh

--- gasp/gasp.py
+++ gasp/gasp.py
@@ -187,6 +187,7 @@ def create_assignment(user):
 @app.route('/hidden/initdb') #can't find a better way to do this
 def initialize_database():
 	init_db()
+	return "success!"
 	
 if __name__=='__main__':
 	app.run(debug=True,host="compsci-dev.pingry.k12.nj.us", port=1030)
